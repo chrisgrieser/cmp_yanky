8af8782af81b650ed4669175a57d50ce6dc3e140
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2023-11-02 16:11:26 +0100

    improv: remove duplicates from suggestions

--- lua/cmp_yanky.lua
+++ lua/cmp_yanky.lua
@@ -13,7 +13,13 @@ function M:complete(request, callback)
 		history = vim.tbl_filter(function(item) return item.filetype == currentFt end, history)
 	end
 
+	local seenItems = {} 
+
 	history = vim.tbl_map(function(item)
+		-- avoid duplicated items showing up
+		if seenItems[item.regcontents] then return end
+		seenItems[item.regcontents] = true
+
 		-- shorten completion text to display
 		local labelMaxLen = 30
 		local label = item.regcontents
